5c56adb690d2b7cd03e51f99a6d7dd3cad29a43f
Kenny <kennychan09870@gmail.com>
2023-05-26 21:12:53 -0700

    google maps api rendered on 'add screen'

--- lib/screens/add_pin_screen.dart
+++ lib/screens/add_pin_screen.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import '../secrets.dart';
 
 class AddPinScreen extends StatefulWidget {
   const AddPinScreen({Key? key}) : super(key: key);
@@ -16,17 +19,54 @@ class AddPinScreenState extends State<AddPinScreen> {
   static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
   static const LatLng destination = LatLng(37.33429383, -122.06600055);
 
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        android_gmaps_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude));
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Track order",
-          style: TextStyle(color: Colors.black, fontSize: 16),
+        centerTitle: true, // Center the app bar title
+        title: const Center(
+          child: Text(
+            "Pin a Location",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ),
       ),
-      body: const Center(
-        child: Text("The Flutter Way!"),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: sourceLocation,
+          zoom: 14.5,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId("source"),
+            position: sourceLocation,
+          ),
+          const Marker(
+            markerId: MarkerId("source"),
+            position: destination,
+          )
+        },
       ),
     );
   }
